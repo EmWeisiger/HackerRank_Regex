31ba83b95d4993ee48f4030b16d0578641a442cd
Em Weisiger <emweisiger@gmail.com>
2016-01-25 13:49:52 -0700

    Create CharacterClass_MatchingRangeOfCharacters.py

---
+++ CharacterClass_MatchingRangeOfCharacters.py
@@ -0,0 +1,7 @@
+# HackerRank_Regex > Character Class > Matching Range Of Characters
+
+Regex_Pattern = r'^[a-z][1-9][^a-z][^A-Z][A-Z]'
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, raw_input()))).lower())
