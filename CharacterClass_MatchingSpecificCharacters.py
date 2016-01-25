d8aeb26e08ea108fe370de9369d9f64b93237047
Em Weisiger <emweisiger@gmail.com>
2016-01-25 13:46:06 -0700

    Create CharacterClass_MatchingSpecificCharacters.py

---
+++ CharacterClass_MatchingSpecificCharacters.py
@@ -0,0 +1,7 @@
+# HackerRank_Regex > Character Class > Matching Specific Characters
+
+Regex_Pattern = r'^[123][120][xs0][30Aa][xsu][.,]$'
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, raw_input()))).lower())
